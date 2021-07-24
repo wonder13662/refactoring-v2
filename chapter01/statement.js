ad0b021f2403766eecad20e85ecdb658da562e5c
wonder13662 <wonder13662@gmail.com>
2021-07-24 13:02:57 +0900

    1.4 statement() 함수 쪼개기 - totalAmount 변수를 인라인

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,4 +1,12 @@
 function statement(invoice, plays) {
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
@@ -49,16 +57,14 @@ function statement(invoice, plays) {
 
     return result;
   }
-
-  let totalAmount = 0;
+  
   let result = `청구내역 (고객명: ${invoice.customer})\n`
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
-    totalAmount += amountFor(perf);
   }
-  
-  result += `총액: ${usd(totalAmount/100)}\n`;
+
+  result += `총액: ${usd(totalAmount()/100)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
