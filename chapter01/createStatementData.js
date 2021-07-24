6622c5a4a0b99f52a00ea7b3dcab565961b66706
wonder13662 <wonder13662@gmail.com>
2021-07-24 17:46:54 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기 - 공연료 계산기 만들기

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.aPerformance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
 export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -7,8 +14,9 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-    result.play = playFor(result);
+    const calculator =  new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
