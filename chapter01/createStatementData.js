11a22b5f0818d7205b5dcfe9fd35b8833f92093c
wonder13662 <wonder13662@gmail.com>
2021-07-24 17:51:37 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기 - PerformanceCalculator 로 amountFor() 함수 옮기기

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -3,6 +3,30 @@ class PerformanceCalculator {
     this.aPerformance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0;
+  
+    switch (this.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+    }
+  
+    return result;    
+  }
 }
 
 export default function createStatementData(invoice, plays) {
