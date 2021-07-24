24e67ce199f419b8be15c617e36a6e36554982f8
wonder13662 <wonder13662@gmail.com>
2021-07-24 13:31:05 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - renderPlainText() 안에서 amountFor()를 호출하던 부분을 중간 데이터를 사용하도록 바꿈

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -6,12 +6,37 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+  
+    switch (aPerformance.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+    }
+  
+    return result;
+  }  
   
   return renderPlainText(statementData, plays);
 }
@@ -20,7 +45,7 @@ function renderPlainText(data, plays) {
   let result = `청구내역 (고객명: ${data.customer})\n`
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += ` ${perf.play.name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
+    result += ` ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience}석)\n`
   }
 
   result += `총액: ${usd(totalAmount()/100)}\n`;
@@ -30,7 +55,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -55,30 +80,6 @@ function renderPlainText(data, plays) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-  
-  function amountFor(aPerformance) {
-    let result = 0;
-  
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-  
     return result;
   }  
 }
