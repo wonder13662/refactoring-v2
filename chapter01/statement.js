f7b13d8d8d353fd7ef05d8f2adbd31c9ada1e41c
wonder13662 <wonder13662@gmail.com>
2021-07-24 12:51:31 +0900

    1.4 statement() 함수 쪼개기 - 반복문 쪼개기

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -45,14 +45,15 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`
-  for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += volumeCreditsFor(perf);
 
+  for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
     totalAmount += amountFor(perf);
   }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }  
 
   result += `총액: ${usd(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
