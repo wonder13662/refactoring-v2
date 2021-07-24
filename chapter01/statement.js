a0b48da69586f008da576935418229c224e9fdaf
wonder13662 <wonder13662@gmail.com>
2021-07-24 13:44:40 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - statement()에 필요한 데이터 처리 코드를 별도 함수로 빼기

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,10 +1,14 @@
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
@@ -58,7 +62,7 @@ function statement(invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구내역 (고객명: ${data.customer})\n`
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
