88951a31bc3a06c9af5ba16a3deec74e4ffc49c1
wonder13662 <wonder13662@gmail.com>
2021-07-24 12:37:43 +0900

    1.4 statement() 함수 쪼개기 - 새로 추출한 함수를 이용해 값을 누적

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,5 +1,12 @@
 function statement(invoice, plays) {
 
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -39,9 +46,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`
