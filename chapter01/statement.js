591818f03615a1f4e05419e82f5b17600af486f3
wonder13662 <wonder13662@gmail.com>
2021-07-24 13:37:11 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - 총합을 구하는 부분 옮김

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -2,6 +2,9 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
@@ -44,9 +47,23 @@ function statement(invoice, plays) {
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
-  }  
-  
-  return renderPlainText(statementData, plays);
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let volumeCredits = 0;
+    for (let perf of data.performances) {
+      volumeCredits += perf.volumeCredits;
+    }
+    return volumeCredits;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -56,25 +73,9 @@ function renderPlainText(data, plays) {
     result += ` ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience}석)\n`
   }
 
-  result += `총액: ${usd(totalAmount()/100)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount/100)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-  
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-    return volumeCredits;
-  }
   
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
