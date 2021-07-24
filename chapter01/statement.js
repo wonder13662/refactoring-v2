6ab5c276f12b2d62fa70ad1b261e6d369ad55b4a
wonder13662 <wonder13662@gmail.com>
2021-07-24 12:44:04 +0900

    1.4 statement() 함수 쪼개기 - format 변수 제거하기

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,4 +1,11 @@
 function statement(invoice, plays) {
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber); 
+  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -38,22 +45,16 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     // 포인트를 적립한다.
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount/100)}\n`;
+  result += `총액: ${usd(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
