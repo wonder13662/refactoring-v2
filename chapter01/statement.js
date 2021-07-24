36fb3ab13c39062edbcac172c0dba14128e46e04
wonder13662 <wonder13662@gmail.com>
2021-07-24 14:01:05 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - html 생성 버전 추가

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -13,15 +13,36 @@ function renderPlainText(data) {
 
   result += `총액: ${usd(data.totalAmount/100)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-  return result;
-  
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber); 
-  }
+  return result;  
 }
 
-module.exports = statement;
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>\n';
+  for(let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`
+    result += `<td>${usd(perf.amount)}</td></tr>`
+  }
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${usd(data.totalVolumeCredits)}</em>점</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber); 
+}
+
+module.exports = {
+  statement,
+  htmlStatement,
+};
--- chapter01/statement.test.js
+++ chapter01/statement.test.js
@@ -1,6 +1,8 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
-const statement = require('./statement');
+const {
+  statement,
+} = require('./statement');
 
 test('연극의 장르와 관객 규모를 기초로 비용을 계산한다', () => {
   let result = '';
