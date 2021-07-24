0aff4322d4720b60cc359b5a97d1f659d55c2942
wonder13662 <wonder13662@gmail.com>
2021-07-24 18:03:53 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기 - PerformanceCalculator 로 amountFor() 함수 옮기기

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -1,6 +1,6 @@
 class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.aPerformance = aPerformance;
+  constructor(performance, aPlay) {
+    this.performance = performance;
     this.play = aPlay;
   }
 
@@ -10,19 +10,19 @@ class PerformanceCalculator {
     switch (this.play.type) {
       case "tragedy": // 비극
         result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
         }
         break;
       case "comedy": // 희극
         result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
         }
-        result += 300 * aPerformance.audience;
+        result += 300 * this.performance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+        throw new Error(`알 수 없는 장르: ${this.play.type}`)
     }
   
     return result;    
@@ -51,27 +51,7 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-  
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-  
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
