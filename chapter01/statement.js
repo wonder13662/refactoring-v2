0cea1e5f18feb0943c9dfe95008a3987baae7af0
wonder13662 <wonder13662@gmail.com>
2021-07-24 13:13:41 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - 본문 전체를 별도 함수로 추출

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -1,4 +1,18 @@
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
+  let result = `청구내역 (고객명: ${invoice.customer})\n`
+  for (let perf of invoice.performances) {
+    // 청구 내역을 출력한다.
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
+  }
+
+  result += `총액: ${usd(totalAmount()/100)}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
   function totalAmount() {
     let result = 0;
     for (let perf of invoice.performances) {
@@ -6,7 +20,7 @@ function statement(invoice, plays) {
     }
     return result;
   }
-
+  
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
@@ -14,7 +28,7 @@ function statement(invoice, plays) {
     }
     return volumeCredits;
   }
-
+  
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -22,21 +36,21 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2,
     }).format(aNumber); 
   }
-
+  
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-
+  
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-
+  
   function amountFor(aPerformance) {
     let result = 0;
-
+  
     switch (playFor(aPerformance).type) {
       case "tragedy": // 비극
         result = 40000;
@@ -54,19 +68,9 @@ function statement(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
     }
-
-    return result;
-  }
   
-  let result = `청구내역 (고객명: ${invoice.customer})\n`
-  for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
-  }
-
-  result += `총액: ${usd(totalAmount()/100)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
+    return result;
+  }  
 }
 
 module.exports = statement;
