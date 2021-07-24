8d0a8e6a33d8412be41f5680db004bdb413ecdf7
wonder13662 <wonder13662@gmail.com>
2021-07-24 12:52:28 +0900

    1.4 statement() 함수 쪼개기 - 문장 슬라이드하기

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -43,14 +43,13 @@ function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`
-
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석)\n`
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }  
