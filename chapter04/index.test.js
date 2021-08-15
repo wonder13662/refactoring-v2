a63c2c17454810805f9c3f2ee068298cd6f55611
Wonder Jung <wonder13662@gmail.com>
2021-08-16 08:52:08 +0900

    [Chapter04]4.6 경계 조건 검사하기

--- chapter04/index.test.js
+++ chapter04/index.test.js
@@ -13,11 +13,63 @@ describe('province', function() {
 
   it('profit', function() {
     expect(asia.profit).toBe(230);
-  })
+  });
 
   it('change production', function() {
     asia.producers[0].production = 20;
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
   })
+
+  it('zero demand', function() { // 수요가 없다.
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+
+  it('negative demand', function() { // 수요가 마이너스다.
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+
+  it('empty string demand', function() { // 수요 입력란이 비어 있다.
+    asia.demand = '';
+    expect(asia.shortfall).toBe(NaN);
+    expect(asia.profit).toBe(NaN);
+  })
+});
+
+describe('no producer', function() { // 생산자가 없다.
+  let noProducers;
+  beforeEach(function() {
+    const data = {
+      name: 'No producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it('shortfall', function() {
+    expect(noProducers.shortfall).toBe(30);
+  });
+
+  it('profit', function() {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+
+describe('string for producers', function() { // 생산자 수 필드에 문자열을 대입한다.
+  it('', function() {
+    const data = {
+      name: 'No producers',
+      producers: '',
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
+  });
 });
