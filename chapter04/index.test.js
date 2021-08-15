21ed3378cb9c108ec5e5ea780c3f3a701b7ba96f
Wonder Jung <wonder13662@gmail.com>
2021-08-16 08:35:06 +0900

    [Chapter04]4.5 픽스처 수정하기

--- chapter04/index.test.js
+++ chapter04/index.test.js
@@ -6,11 +6,18 @@ describe('province', function() {
   beforeEach(function() {
     asia = new Province(sampleProvinceData());
   });
+
   it('shortfall', function() {
-    expect(asia.shortfall).toBe(5); // 2. 검증
+    expect(asia.shortfall).toBe(5);
   });
 
   it('profit', function() {
     expect(asia.profit).toBe(230);
+  })
+
+  it('change production', function() {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
   })
 });
